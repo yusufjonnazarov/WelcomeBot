39aa4f92867a9c1e2d54e906a2018808099f2086
Yusuf <52450380+yusufjonnazarov@users.noreply.github.com>
2020-11-06 13:12:54 +0500

    Update bot.js

--- bot/bot.js
+++ bot/bot.js
@@ -4,36 +4,33 @@ const { Telegraf } = require('telegraf')
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
 bot.use(async (_, next) => {
-    const start = new Date()
-    await next()
-    const ms = new Date() - start
-    console.log('Response time: %sms', ms)
+    try {
+        const start = new Date()
+        await next()
+        const ms = new Date() - start
+        console.log('Response time: %sms', ms)
+    }
+    catch (error) {
+        console.log(`Error Occured: ${error.message}`)
+    }
 })
 
 module.exports = fp((fastify, _, next) => {
 
-    bot.on('new_chat_members', (ctx) => {
-        try {
-            if (ctx.message.chat.id == process.env.GROUP_ID) {
-                try { ctx.deleteMessage() }
-                catch (err) { }
-                for (const member of ctx.message.new_chat_members) {
-                    try { ctx.reply(process.env.WELCOME_MESSAGE.replace('{{name}}', member.first_name)) }
-                    catch (err) { }
-                }
+    bot.on('new_chat_members', async (ctx) => {
+        if (ctx.message.chat.id == process.env.GROUP_ID) {
+            await ctx.deleteMessage();
+            for (const member of ctx.message.new_chat_members) {
+                await ctx.reply(process.env.WELCOME_MESSAGE.replace('{{name}}', member.first_name))
             }
-        } catch (error) { }
+        }
     })
 
-    bot.on('left_chat_member', (ctx) => {
-        try {
-            if (ctx.message.chat.id == process.env.GROUP_ID) {
-                try {
-                    ctx.deleteMessage()
-                    ctx.reply(process.env.LEFT_MESSAGE.replace('{{name}}', ctx.message.left_chat_member.first_name))
-                } catch (error) { }
-            }
-        } catch (error) { }
+    bot.on('left_chat_member', async (ctx) => {
+        if (ctx.message.chat.id == process.env.GROUP_ID) {
+            await ctx.deleteMessage()
+            await ctx.reply(process.env.LEFT_MESSAGE.replace('{{name}}', ctx.message.left_chat_member.first_name))
+        }
     })
 
     bot.launch()
